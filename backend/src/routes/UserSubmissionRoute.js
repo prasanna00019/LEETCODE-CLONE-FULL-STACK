c727bd6fbe0c8cb5e3d2912b47b29a744eaf48eb
prasanna00019 <159623027+prasanna00019@users.noreply.github.com>
2024-10-17 17:14:01 +0530

    Update UserSubmissionRoute.js

--- backend/src/routes/UserSubmissionRoute.js
+++ backend/src/routes/UserSubmissionRoute.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { getSubmission, submitCode } from "../controllers/UserSubmissionController.js";
+import { getAllOfParticularUser, getSubmission, submitCode } from "../controllers/UserSubmissionController.js";
 const UserSubmissionRouter=express.Router();
 UserSubmissionRouter.post('/',submitCode);
 UserSubmissionRouter.get('/:userId/:probId',getSubmission);
+UserSubmissionRouter.get('/:userId',getAllOfParticularUser);
 export default UserSubmissionRouter;
